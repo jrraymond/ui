e9c65cc1087b7cc77b0b3b7de7ea56002f3767df
Justin Raymond <justin.raymond.14@gmail.com>
2014-01-07 15:51:32 -0500

    warming animations, volume button styling, moving away from checkbuttons

--- js/main.js
+++ js/main.js
@@ -1,105 +1,121 @@
 var srcs = Object.freeze({ pc: {}, mac: {}, hdmi: {}, vga: {}, dvd: {}, bluray: {} });
-var outs = { out1 : { name: 'west', source: undefined, on: false, vm: false },
-             out2 : { name: 'east', source: undefined, on: false, vm: false },
-             out3 : { name: 'north' , source: undefined, on: false, vm: false },
-             active : 1 };
+var outs = { active : 1,
+             out1 : { name: 'west', source: undefined, on: false, vm: false },
+             out2 : { name: 'east', source: 'mac', on: false, vm: false },
+             out3 : { name: 'north' , source: 'mac', on: false, vm: false } };
 
 
 /* POWER BUTTON LISTENER */
-$(document).on("click", ".pwr-btn > input", function() {
-  var current = this.dataset.output;
-  if ($(this).prop("checked")) {
-    console.log("power on for "+current);
-    $(".cube").removeClass("c-top");
-    outs[current].on = true;
+$(document).on('click', '.pwr-btn > label', function() {
+  var current = outs['out'+outs.active];
+  var that = this;
+  if (!$(this).hasClass('pwr-on')) {
+    var blink = setInterval(warming, 500);
+    window.setTimeout(clearInterval, 5000, blink);
+    window.setTimeout(powerOn, 5000, that);
+    current.on = true;
   }
   else {
-    console.log("power off for"+current);
-    $(".cube").addClass("c-top");
-    outs[current].on = false;
+    $(this).removeClass('pwr-on');
+    console.log('power off for '+outs.active);
+    $('.p-'+outs.active+' .cube').addClass('c-top');
+    current.on = false;
   }
-  
 });
+var warming = function() {
+  var label = $('.p-'+outs.active+' .pwr-btn > label');
+  console.log();
+  if (label.hasClass('warming')) {
+    console.log('blink off');
+    label.removeClass('warming');
+  }
+  else {
+    console.log('blink on');
+    label.addClass('warming')
+  }
+}
+var powerOn = function(that) {
+  console.log('power on for '+outs.active);
+  $(that).removeClass('warming');
+  $(that).addClass('pwr-on');
+  $('.p-'+outs.active+' .cube').removeClass('c-top');
+}
 /* VIDEO MUTE LISTENER */
-$(document).on("click",".vm-btn > input", function() {
+$(document).on('click','.vm-btn > label', function() {
   var current = this.dataset.output;
-  if ($(this).prop("checked")) {
-    console.log("video muted for "+current);
-    $(".cube").addClass("cube-vm");
+  if (!$(this).hasClass('vm-on')) {
+    console.log('video muted for '+current);
+    $(this).addClass('vm-on');
+    $('.cube').addClass('cube-vm');
     outs[current].vm = true;
   }
   else {
-    console.log("vidoe unmuted");
-    $(".cube").removeClass("cube-vm");
+    console.log('vidoe unmuted');
+    $(this).removeClass('vm-on');
+    $('.cube').removeClass('cube-vm');
     outs[current].vm = false;
-    console.log("video muted for "+current);
+    console.log('video muted for '+current);
   }
 });
 /* TAB NAVIGATION */
-$(document).on("click", ".out-nav-item", function() {
-  var oldOut = $(".nav-selected").data() ? $(".nav-selected").data().tab : "none";
-  $(".nav-selected").removeClass("nav-selected");
-  $(this).addClass("nav-selected");
-  $(".active").removeClass("active");
-  $("#" + this.dataset.tab).addClass("active");
-  $("#p-cube").removeClass("p-" + oldOut);
-  $("#p-cube").addClass("p-" + this.dataset.tab);
+$(document).on('click', '.out-nav-item', function() {
+  var oldOut = $('.nav-selected').data() ? $('.nav-selected').data().tab : 1;
+  $('.nav-selected').removeClass('nav-selected');
+  $(this).addClass('nav-selected');
+  $('.active').removeClass('active');
+  $('#out' + this.dataset.tab).addClass('active');
+  $('#p-cube').removeClass('p-out-' + oldOut);
+  $('#p-cube').addClass('p-out-' + this.dataset.tab);
   outs.active = this.dataset.tab;
-  updateState();
+  $('#src-' + outs['out'+outs.active].source).click();
+  console.log(outs);
 });
 
-var updateState = function() {
-  console.log("state updated");
-};
 
 /* SOURCE SELECTION LISTENER */
-$(document).on("click", ".src", function() {
-  var source = $(this).children("label").children(":last-child").html();
-  console.log('switching to '+source);
-  selectSource(source);
-  var newClass = getMatrixClass(source);
-  $(".cube").removeClass("c-top c-front c-left c-right c-back c-bottom").addClass(newClass);
+$(document).on('click', '.src', function() {
+  var cur = outs.active;
+  var s = $(this).children('label').children(':last-child').html();
+  console.log('switching '+cur+' to '+s);
+  $('#src-' + s + ' input').prop('checked', true);
+  var newClass = getMatrixClass(s);
+  $('.p-'+cur+' .cube').removeClass('c-top c-front c-left c-right c-back c-bottom').addClass(newClass);
+  outs['out'+cur].source = s;
 });
 
-/* SELECTS SOURCE BUTTON */
-function selectSource(s) {
-  $("#src-" + s + " input").prop('checked', true);
-}
 
 /* Returns correct class */
 function getMatrixClass(s) {
   switch(s) {
-    case "mac":
-      return "c-front";
+    case 'mac':
+      return 'c-front';
       break;
-    case "pc":
-      return "c-bottom";
+    case 'pc':
+      return 'c-bottom';
       break;
-    case "vga":
-      return "c-back";
+    case 'vga':
+      return 'c-back';
       break;
-    case "dvd":
-      return "c-right";
+    case 'dvd':
+      return 'c-right';
       break;
-    case "hdmi":
-      return "c-left";
+    case 'hdmi':
+      return 'c-left';
       break;
-    case "bluray":
-      return "c-top"
+    case 'bluray':
+      return 'c-top'
       break;
   }
 }
 
 
-
-$(".cube").addClass("c-top");
-$('#switch_mute').click();
-$('#switch_mute').click();
-$('#switchW1').click();
+/* Initial configuration */
+$('.cube').addClass('c-top');
+$('.out-nav-item')[0].click();
 
 
 /* Prevent dragging of images */
-$("img").on("dragstart", function(event) {
+$('img').on('dragstart', function(event) {
   event.preventDefault();
 }); 
 
@@ -109,47 +125,47 @@ $("img").on("dragstart", function(event) {
 var findMonth = function(num) {
   switch (num) {
     case 0 :
-      return "Jan";
+      return 'Jan';
     case 1 :
-      return "Feb";
+      return 'Feb';
     case 2 :
-      return "Mar";
+      return 'Mar';
     case 3 :
-      return "Apr";
+      return 'Apr';
     case 4 :
-      return "May";
+      return 'May';
     case 5 :
-      return "Jun";
+      return 'Jun';
     case 6 :
-      return "Jul";
+      return 'Jul';
     case 7 :
-      return "Aug";
+      return 'Aug';
     case 8 :
-      return "Sep";
+      return 'Sep';
     case 9 :
-      return "Oct";
+      return 'Oct';
     case 10 :
-      return "Nov";
+      return 'Nov';
     case 11 :
-      return "Dec";
+      return 'Dec';
   }
 }
 var daySuffix = function(num) {
   switch (num) {
     case (num === 1 || num === 21 || num === 31) : 
-      return "st";
+      return 'st';
     case (num === 2 || num === 22) : 
-      return "nd";
+      return 'nd';
     case (num === 3 || num === 23) :
-      return "rd";
+      return 'rd';
     default :
-      return "th";
+      return 'th';
   }
 }
 
 /* Pads single digits with zeros */
 var pad = function(x) {
-  return x < 10 ? "0"+x : x;
+  return x < 10 ? '0'+x : x;
 };
 
 /* converts hours in military to tuple of hours, am/pm */
@@ -157,10 +173,10 @@ var fromMilitary = function(hours) {
   if (hours >= 12) {
     hours = hours - 12;
     hours = (hours === 0) ? 12 : hours;
-    return [hours, "pm"];
+    return [hours, 'pm'];
   }
   else {
-    return [hours, "am"];
+    return [hours, 'am'];
   }
 }
 
@@ -178,50 +194,50 @@ var updateTime = function() {
   var day = d.getDate();
   var daySuff = daySuffix(day);
 
-  $("#clock").html(h2[0] + ":" + m + ":" + s + " " + h2[1]);
+  $('#clock').html(h2[0] + ':' + m + ':' + s + ' ' + h2[1]);
 
 
-  $("#month").html(month + " ");
-  $("#day").html(day);
-  $("#daySuffix").html(daySuff);
+  $('#month').html(month + ' ');
+  $('#day').html(day);
+  $('#daySuffix').html(daySuff);
 };
 /* calls updateTime every second */
 updateTime();
 setInterval(updateTime, 1000);
 
 /* VOLUME KNOB MUTE CONTROL */
-$(document).on("click","#vol-mute", function() {
-  if (!$(this).hasClass("vol-muted-m")) {
-    $(this).addClass("vol-muted-m");
-    $("#vol-switches > label").each(function() {
-      $(this).hide();
-      //if ($(this).children("div").children("div").hasClass("vol-checked")) {
-       // $(this).children("div").children("div").addClass("vol-muted")
-      //}
+$(document).on('click','#vol-mute', function() {
+  if (!$(this).hasClass('vol-muted-m')) {
+    $(this).addClass('vol-muted-m');
+    $('#vol-switches > label').each(function() {
+      $(this).children('div').children('div').addClass('vol-muted')
+      if ($(this).children('div').children('div').hasClass('vol-checked')) {
+        $(this).children('div').children('div').addClass('vol-checked-muted')
+      }
     });
-    $("input[name='switch']").attr('disabled',true);
+    $('input[name="switch"]').attr('disabled',true);
   }
   else {
-    $(this).removeClass("vol-muted-m");
-    $("#vol-switches > label").each(function() {
-      $(this).show();
-      //if ($(this).children("div").children("div").hasClass("vol-checked")) {
-       // $(this).children("div").children("div").removeClass("vol-muted")
-      //}
+    $(this).removeClass('vol-muted-m');
+    $('#vol-switches > label').each(function() {
+      $(this).children('div').children('div').removeClass('vol-muted')
+      if ($(this).children('div').children('div').hasClass('vol-checked')) {
+        $(this).children('div').children('div').removeClass('vol-checked-muted')
+      }
     });
-    $("input[name='switch']").attr('disabled',false);
+    $('input[name="switch"]').attr('disabled',false);
   }
 });
   /* VOLUME KNOB LEVEL CONTROL */
-$(document).on("click", ".vol-c", function() {
+$(document).on('click', '.vol-c', function() {
   var label = $(this).parent().parent();
-  if (!$("#vol-mute").hasClass("vol-muted-m")) {
-    label.nextAll("label").each(function() {
-      $(this).children("div").children("div").removeClass("vol-checked");
+  if (!$('#vol-mute').hasClass('vol-muted-m')) {
+    label.nextAll('label').each(function() {
+      $(this).children('div').children('div').removeClass('vol-checked');
     });
-    label.prevAll("label").not("#switch-m-l").each(function() {
-      $(this).children("div").children("div").addClass("vol-checked");
+    label.prevAll('label').not('#switch-m-l').each(function() {
+      $(this).children('div').children('div').addClass('vol-checked');
     });
-    $(this).addClass("vol-checked");
+    $(this).addClass('vol-checked');
   }
 });
