c70ebc52ce5325e112b810cf4143e7896393ddd0
Justin Raymond <justin.raymond.14@gmail.com>
2014-01-08 15:58:32 -0500

    dynamically add outputs

--- js/main.js
+++ js/main.js
@@ -1,13 +1,127 @@
-var srcs = Object.freeze({ pc: {}, mac: {}, hdmi: {}, vga: {}, dvd: {}, bluray: {} });
-var outs = { active : 1,
-             out1 : { name: 'west', source: undefined, on: false, vm: false },
-             out2 : { name: 'east', source: 'mac', on: false, vm: false },
-             out3 : { name: 'north' , source: 'mac', on: false, vm: false } };
+var SOURCE = Object.freeze({ PC: 'pc', MAC: 'mac', HDMI: 'hdmi', VGA: 'vga', DVD: 'dvd', BLURAY: 'bluray' });
+var OUTPUT = Object.freeze({ PROJECTOR: 'projector', TELEVISION: 'television' });
 
+/* DYNAMICALLY CREATE PROJECTORS AND SOURCES */
+var sources = [SOURCE.PC, SOURCE.MAC];
+var outs = { 1: { name: 'west', type: OUTPUT.PROJECTOR, source: undefined, on: false, vm: false },
+             2: { name: 'east', type: OUTPUT.PROJECTOR, source: undefined, on: false, vm: false },
+             3: { name: 'north', type: OUTPUT.TELEVISION, source: undefined, on: false, vm: false } };
+var state = { active: 1};
+
+var getSourceIcon = function(s) {
+  switch(s) {
+    case SOURCE.HDMI:
+      return 'cf icon-hdmi';
+      break;
+    case SOURCE.VGA:
+      return 'cf icon-vga';
+      break;
+    case SOURCE.MAC:
+      return 'fa fa-apple';
+      break;
+    case SOURCE.PC:
+      return 'fa fa-windows';
+      break;
+    case SOURCE.DVD:
+      return 'cf icon-dvd';
+      break;
+    default:
+      return 'cf icon-cmdr';
+  }
+}
+var getOutputIcon = function(o) {
+  switch(o) {
+    case OUTPUT.PROJECTOR:
+      return 'fa fa-video-camera';
+      break;
+    case OUTPUT.TELEVISION:
+      return 'fa fa-desktop';
+      break;
+    default:
+      return 'fa fa-video-camera';
+  }
+}
+var addSources = function(sources) {
+  var html = '';
+  for (var s in sources) {
+    if (sources.hasOwnProperty(s)) {
+      html += '<div class="src" id="src-'+sources[s]+'">'+
+                '<input type="radio" name="source">'+
+                '<label>'+
+                  '<div><i class="'+getSourceIcon(sources[s])+'"></i></div>'+
+                  '<div>'+sources[s]+'</div>'+
+                '</label>'+
+              '</div>';
+    }
+  }
+  $('#src-group').html(html);
+}
+
+addSources(sources);
+
+var addOutputs = function(outputs) {
+  var navHtml = '';
+  var octHtml = '';
+  var c = 1;
+  for (var o in outputs) {
+    if (outputs.hasOwnProperty(o)) {
+      navHtml += '<div class="col-xs-4">'+
+                '<div class="out-nav-item" data-tab="'+o+'">'+
+                  '<i class="'+getOutputIcon(outputs[o].type)+'"></i>'+
+                  '<span>'+outputs[o].name+'</span>'+
+                '</div>'+
+              '</div>';
+      octHtml = '<div class="col-xs-6 out-info">'+
+                  '<div class="transforms">'+
+                    '<div class="cube p-front">'+
+                      '<div class="face f-top">'+
+                        '<i class="cf icon-cmdr fa-7x"></i>'+
+                      '</div>'+
+                      '<div class="face f-front">'+
+                        '<i class="fa fa-apple fa-7x"></i>'+
+                      '</div>'+
+                      '<div class="face f-bottom">'+
+                        '<i class="fa fa-windows fa-7x"></i>'+
+                      '</div>'+
+                      '<div class="face f-back">'+
+                        '<i class="cf icon-vga fa-7x"></i>'+
+                      '</div>'+
+                      '<div class="face f-left">'+
+                        '<i class="cf icon-hdmi fa-7x"></i>'+
+                      '</div>'+
+                      '<div class="face f-right">'+
+                        '<i class="cf icon-dvd fa-7x"></i>'+
+                      '</div>'+
+                    '</div>'+
+                  '</div>'+
+                '</div>'+
+                '<div class="col-xs-3">'+
+                  '<div class="pwr-btn">'+
+                    '<label>'+
+                      '<i class="fa fa-power-off fa-3x"></i>'+
+                    '</label>'+
+                  '</div>'+
+                '</div>'+
+                '<div class="col-xs-3">'+
+                  '<div class="vm-btn">'+
+                    '<label>'+
+                      '<span class="vm-txt">VIDEO MUTE</span>'+
+                    '</label>'+
+                  '</div>'+
+                '</div>';
+      $('#o-'+c).html(octHtml);
+      console.log($('#o-'+c));
+      c++;
+    }
+  }
+  $('#out-nav').html(navHtml);
+}
+
+addOutputs(outs);
 
 /* POWER BUTTON LISTENER */
 $(document).on('click', '.pwr-btn > label', function() {
-  var current = outs['out'+outs.active];
+  var current = outs[state.active];
   var that = this;
   if (!$(this).hasClass('pwr-on')) {
     var blink = setInterval(warming, 500);
@@ -17,13 +131,13 @@ $(document).on('click', '.pwr-btn > label', function() {
   }
   else {
     $(this).removeClass('pwr-on');
-    console.log('power off for '+outs.active);
-    $('.p-'+outs.active+' .cube').addClass('c-top');
+    console.log('power off for '+state.active);
+    $('#o-'+state.active+' .cube').addClass('c-top');
     current.on = false;
   }
 });
 var warming = function() {
-  var label = $('.p-'+outs.active+' .pwr-btn > label');
+  var label = $('#o-'+state.active+' .pwr-btn > label');
   console.log();
   if (label.hasClass('warming')) {
     console.log('blink off');
@@ -35,25 +149,25 @@ var warming = function() {
   }
 }
 var powerOn = function(that) {
-  console.log('power on for '+outs.active);
+  console.log('power on for '+state.active);
   $(that).removeClass('warming');
   $(that).addClass('pwr-on');
-  $('.p-'+outs.active+' .cube').removeClass('c-top');
+  $('#o-'+state.active+' .cube').removeClass('c-top');
 }
 /* VIDEO MUTE LISTENER */
 $(document).on('click','.vm-btn > label', function() {
-  var current = outs.active;
+  var current = state.active;
   if (!$(this).hasClass('vm-on')) {
     console.log('video muted for '+current);
     $(this).addClass('vm-on');
     $('.cube').addClass('cube-vm');
-    outs['out'+current].vm = true;
+    outs[current].vm = true;
   }
   else {
     console.log('vidoe unmuted');
     $(this).removeClass('vm-on');
     $('.cube').removeClass('cube-vm');
-    outs['out'+current].vm = false;
+    outs[current].vm = false;
   }
 });
 /* TAB NAVIGATION */
@@ -63,23 +177,30 @@ $(document).on('click', '.out-nav-item', function() {
   $(this).addClass('nav-selected');
   $('.active').removeClass('active');
   $('#out' + this.dataset.tab).addClass('active');
-  $('#p-cube').removeClass('p-out-' + oldOut);
-  $('#p-cube').addClass('p-out-' + this.dataset.tab);
-  outs.active = this.dataset.tab;
-  $('#src-' + outs['out'+outs.active].source).click();
+  $('#octagon').removeClass('p-out-' + oldOut);
+  $('#octagon').addClass('p-out-' + this.dataset.tab);
+  state.active = this.dataset.tab;
+  var nextSource = outs[state.active].source;
+  if (nextSource) {
+    $('#src-'+nextSource).click();
+  }
+  else {
+    $('.src > input').prop('checked', false);
+  }
+
   console.log(outs);
 });
 
 
 /* SOURCE SELECTION LISTENER */
 $(document).on('click', '.src', function() {
-  var cur = outs.active;
+  var cur = state.active;
   var s = $(this).children('label').children(':last-child').html();
   console.log('switching '+cur+' to '+s);
   $('#src-' + s + ' input').prop('checked', true);
   var newClass = getMatrixClass(s);
-  $('.p-'+cur+' .cube').removeClass('c-top c-front c-left c-right c-back c-bottom').addClass(newClass);
-  outs['out'+cur].source = s;
+  $('#o-'+cur+' .cube').removeClass('c-top c-front c-left c-right c-back c-bottom').addClass(newClass);
+  outs[cur].source = s;
 });
 
 
@@ -240,3 +361,4 @@ $(document).on('click', '.vol-c', function() {
     $(this).addClass('vol-checked');
   }
 });
+
