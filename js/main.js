c743b5c95819959c5022a70173ac2e0de3e7982e
Justin Raymond <justin.raymond.14@gmail.com>
2014-03-30 13:18:41 -0400

    added time

--- js/main.js
+++ js/main.js
@@ -25,13 +25,13 @@ var onDragOver = function(e) {
 };
 var onDragEnter = function(e) {
   'use strict';
-  this.classList.add('over');
+  this.classList.add('drag-over');
   //console.log('drag enter');
   //console.log(e);
 };
 var onDragLeave = function(e) {
   'use strict';
-  this.classList.remove('over');
+  this.classList.remove('drag-over');
   //console.log('drag leave');
   //console.log(e);
 };
@@ -61,3 +61,90 @@ $(document).on('dragenter', '.src', onDragEnter);
 $(document).on('dragleave', '.src', onDragLeave);
 $(document).on('drop', '.src', onDrop);
 $(document).on('dragend', '.src', onDragEnd);
+
+/* SETS DATE */
+var findMonth = function(num) {
+  'use strict';
+  switch (num) {
+    case 0 :
+      return 'Jan';
+    case 1 :
+      return 'Feb';
+    case 2 :
+      return 'Mar';
+    case 3 :
+      return 'Apr';
+    case 4 :
+      return 'May';
+    case 5 :
+      return 'Jun';
+    case 6 :
+      return 'Jul';
+    case 7 :
+      return 'Aug';
+    case 8 :
+      return 'Sep';
+    case 9 :
+      return 'Oct';
+    case 10 :
+      return 'Nov';
+    case 11 :
+      return 'Dec';
+  }
+};
+var daySuffix = function(num) {
+  'use strict';
+  switch (num) {
+    case (num === 1 || num === 21 || num === 31) :
+      return 'st';
+    case (num === 2 || num === 22) :
+      return 'nd';
+    case (num === 3 || num === 23) :
+      return 'rd';
+    default :
+      return 'th';
+  }
+};
+
+/* Pads single digits with zeros */
+var pad = function(x) {
+  'use strict';
+  return x < 10 ? '0'+x : x;
+};
+
+/* converts hours in military to tuple of hours, am/pm */
+var fromMilitary = function(hours) {
+  'use strict';
+  if (hours >= 12) {
+    hours = hours - 12;
+    hours = (hours === 0) ? 12 : hours;
+    return [hours, 'pm'];
+  }
+  else {
+    return [hours, 'am'];
+  }
+};
+/* updates the clock */
+var updateTime = function() {
+  'use strict';
+  var d = new Date();
+
+  var h = pad(d.getHours());
+  var m = pad(d.getMinutes());
+  var s = pad(d.getSeconds());
+
+  var h2 = fromMilitary(h);
+
+  var month = findMonth(d.getMonth());
+  var day = d.getDate();
+  var daySuff = daySuffix(day);
+
+  $('#clock').html(h2[0] + ':' + m + ':' + s + ' ' + h2[1]);
+
+
+  $('#month').html(month + ' ');
+  $('#day').html(day);
+  $('#daySuffix').html(daySuff);
+};
+updateTime();
+setInterval(updateTime, 1000);
